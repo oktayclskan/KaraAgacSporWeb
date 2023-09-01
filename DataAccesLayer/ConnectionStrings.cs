a35b8c607ba0402e57c2323929538954781d60ed
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2023-09-01 17:23:25 +0300

    is continued

--- DataAccesLayer/ConnectionStrings.cs
+++ DataAccesLayer/ConnectionStrings.cs
@@ -11,5 +11,4 @@ namespace DataAccesLayer
         //public static string ConStr = @"data source =.\SQLEXPRESS;Integrated Security = SSPI; AttachDBFilename=|DataDirectory|aspnetdb.mdf;User Instance = true";
         public static string ConStr = @"Data Source=94.199.202.242;Network Library=DBMSSOCN;Initial Catalog=hhbadakd_Karaagac;User ID=appsolypreparation;Password=493052Hhb-";
     }
-    
 }
